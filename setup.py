60c7ebdcfc31f6722e4bd1d0d6a0ebea4c9110e4
Evan Broder <broder@mit.edu>
2008-10-30 09:26:02 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="SimpleStar",
+    version="0.0.1",
+    description="SimpleStar - a simple library for Asterisk AGI scripts",
+    author="Evan Broder",
+    author_email="broder@mit.edu",
+    #url="http://ebroder.net/code/SimpleStar",
+    license="MIT",
+    packages=['simplestar'],
+)
